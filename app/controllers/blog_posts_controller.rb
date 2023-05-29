a0201f107b84adbdec5f05179ae920b482185680
Megan Ratto <94590526+rattomegan@users.noreply.github.com>
2023-05-28 22:13:09 -0400

    Add new blog post

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -12,4 +12,27 @@ class BlogPostsController < ApplicationController
     # this is a helper method that equates to "/"
     redirect_to root_path
   end
+
+  def new
+    # creates a blog_post in memory, but not the database
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    # rails does not allow you to pass params directly to the model for security reasons
+    # @blog_post = BlogPost.new(params[:blog_post])
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      # status gives us the correct response code in our http request
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
 end
