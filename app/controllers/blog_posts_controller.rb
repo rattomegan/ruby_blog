6f8aa2c218afbe4a168bb8fe61a468ae92a69233
Megan Ratto <94590526+rattomegan@users.noreply.github.com>
2023-05-28 21:43:15 -0400

    Add show route

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,4 +4,12 @@ class BlogPostsController < ApplicationController
     # rails knows to share these with the erb templates
     @blog_posts = BlogPost.all
   end
+
+  def show
+    # params comes from ApplicationController
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    # this is a helper method that equates to "/"
+    redirect_to root_path
+  end
 end
