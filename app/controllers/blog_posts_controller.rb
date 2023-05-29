7d41199534357c89997c7e6ac48a08cdf196cbdb
Megan Ratto <94590526+rattomegan@users.noreply.github.com>
2023-05-29 11:12:05 -0400

    Add delete and refactor routes, controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,4 +1,7 @@
 class BlogPostsController < ApplicationController
+  # this helper method can replace the .find()
+  before_action :set_blog_post, except: [:index, :new, :create] # only: [:show, :edit, :update, :destroy]
+
   def index
     # look up all posts and save to an instance variable
     # rails knows to share these with the erb templates
@@ -6,11 +9,6 @@ class BlogPostsController < ApplicationController
   end
 
   def show
-    # params comes from ApplicationController
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    # this is a helper method that equates to "/"
-    redirect_to root_path
   end
 
   def new
@@ -31,11 +29,9 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -43,9 +39,21 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    # this is a helper method that equates to "/"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update" # you don't need a name here - it will look at the show route url - it doesn't care about the request type
-  get "/blog_posts/:id/edit/", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  #this can replace all the routes we manually defined
+  resources :blog_posts
+  
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update" # you don't need a name here - it will look at the show route url - it doesn't care about the request type
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "/blog_posts/:id/edit/", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
