de4759d13b42d5cab261180c787a7a6b435181ec
Megan Ratto <94590526+rattomegan@users.noreply.github.com>
2023-06-04 19:22:49 -0400

    Add actiontext

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,9 @@
 class BlogPost < ApplicationRecord
+  # This goes in a separate database table and it sets up association between the two tables
+  has_rich_text :content
+
   validates :title, presence: true
-  validates :body, presence: true
+  validates :content, presence: true
 
 # second argument is secondary sorting
   scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc)}
